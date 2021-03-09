6f0ae1d45fcfcd4fad8012ca179fe688baf9b70d
zzzz <1505723938@qq.com>
2021-03-09 17:15:35 +0800

    1

---
+++ example/android/app/src/main/java/com/weilu/flutter/flutter_2d_amap_example/MainActivity.java
@@ -0,0 +1,12 @@
+package com.weilu.flutter.flutter_2d_amap_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
